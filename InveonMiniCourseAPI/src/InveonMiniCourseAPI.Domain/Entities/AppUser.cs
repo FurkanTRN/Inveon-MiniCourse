2446560b464d48c1ca5d350e30184ddc242b559c
TRN <contact.furkantrn@gmail.com>
2024-12-30 00:42:22 +0300

    Add additional properties to AppUser

--- InveonMiniCourseAPI/src/InveonMiniCourseAPI.Domain/Entities/AppUser.cs
+++ InveonMiniCourseAPI/src/InveonMiniCourseAPI.Domain/Entities/AppUser.cs
@@ -4,5 +4,9 @@ namespace InveonMiniCourseAPI.Domain.Entities;
 
 public class AppUser : IdentityUser<long>
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? AvatarPath { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     
 }
